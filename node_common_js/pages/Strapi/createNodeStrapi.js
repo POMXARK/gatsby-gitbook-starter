f78525de3efeda6daa3918934ddcee58c753421c
Роман Бушуев <bushuev@benefitty.ru>
2024-06-23 21:29:59 +0400

    feat: add sidebar sub-category strapi
    Добавил поддержку вложенной категории в боковое меню для strapi.

--- node_common_js/pages/Strapi/createNodeStrapi.js
+++ node_common_js/pages/Strapi/createNodeStrapi.js
@@ -5,12 +5,6 @@ exports.createNodeStrapi = (node, getNode, actions, createNodeField) => {
     const title = matter(children.article).data.title
 
     createNodeField({
-      name: `slug`,
-      node,
-      value: `/${title.toLowerCase()}`,
-    });
-
-    createNodeField({
       name: 'id',
       node,
       value: node.id,
@@ -20,5 +14,21 @@ exports.createNodeStrapi = (node, getNode, actions, createNodeField) => {
       name: 'title',
       node,
       value: title,
+    });
+
+    let slug = '/' + title.replace(/\s+/g, '-').toLowerCase();
+
+    if (node.category___NODE) {
+      let categories = '/' + getNode(node.category___NODE).name.replace(/\s+/g, '-').toLowerCase();
+      if (node.sub_category___NODE) {
+        categories += '/' + getNode(node.sub_category___NODE).name.replace(/\s+/g, '-').toLowerCase()
+      }
+      slug = categories + slug
+    }
+
+    createNodeField({
+      name: `slug`,
+      node,
+      value: slug,
     });
 }
